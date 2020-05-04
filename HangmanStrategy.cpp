daf6858bbcf20829666f2cbfa82b1f13ca4c3cd1
Jacob Faulk <jacob5567@gmail.com>
2020-05-03 23:31:50 -0700

    checks all words the word could be

--- HangmanStrategy.cpp
+++ HangmanStrategy.cpp
@@ -5,7 +5,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-// #include <omp.h>
+#include <omp.h>
 
 using namespace std;
 
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
     cout << "num threads: " << thread_count << endl;
 
     vector<string> words;
-    string word;
+    string wordBuffer;
     cout << words.max_size() << endl;
     words.reserve(380000);
     ifstream dictionaryFile;
@@ -31,10 +31,10 @@ int main(int argc, char *argv[])
     {
         while (!dictionaryFile.eof())
         {
-            getline(dictionaryFile, word);
-            if (!word.empty() && word[word.size() - 1] == '\r')
-                word.erase(word.size() - 1);
-            words.push_back(word);
+            getline(dictionaryFile, wordBuffer);
+            if (!wordBuffer.empty() && wordBuffer[wordBuffer.size() - 1] == '\r')
+                wordBuffer.erase(wordBuffer.size() - 1);
+            words.push_back(wordBuffer);
             i++;
         }
     }
@@ -45,6 +45,53 @@ int main(int argc, char *argv[])
     }
 
     dictionaryFile.close();
+
+    // Working through words
+    for (string word : words)
+    {
+        int len = word.length();
+        // TODO number of guesses + extract this whole thing into a loop
+
+        string revealedText = "";
+        for (int i = 0; i < len; i++)
+        {
+            revealedText.push_back('_');
+        }
+
+        vector<char> letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        vector<char> guessed;
+        vector<string> possibleWords;
+        possibleWords.reserve(10000);
+
+        // all words it could be by length
+        #pragma omp parallel for num_threads(thread_count)
+        for (int i = 0; i < words.size(); i++)
+        {
+            // length check
+            if (words[i].length() != len)
+                continue;
+
+            // check if it contains a letter that was already guessed
+            for (char g : guessed) // TODO change this to just check the previously guessed letter
+            {
+                if(words[i].find(g))
+                    continue;
+            }
+
+            // check specific letter locations
+            for (int j = 0; j < len; j++)
+            {
+                if(revealedText[j] != '_' && revealedText[j] != words[i][j])
+                    continue;
+            }
+
+            // add word as possible
+            #pragma omp critical
+            {
+                possibleWords.push_back(words[i]);
+            }
+        }
+    }
 
     return 0;
 }
